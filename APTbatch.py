a8cb59dae1f373ba14a21a655b99e331751b3890
David Campbell <d.a.campbell2@herts.ac.uk>
2019-07-22 10:42:43 +0100

    Added comments

--- APTbatch.py
+++ APTbatch.py
@@ -1,20 +1,47 @@
+##### ----APTbatch.py---#############################################################################
+#																									#
+# Runs Aperture Photometry Tool (APT) to create a source list for every FITS file in a directory	#
+#																									#
+#####################################################################################################
+
 import subprocess
 import os
 
+#-----------Variables to be customised before use-----------------------------------------------------
+
+
+#Directory containing the FITS images you want processed
 directory = r"C:\Users\David\Documents\apt tests\Transit 1"
-aptJar = "C:/APT/APT.jar"
+
+#Location of the APT.jar file
+aptJar = r"C:\APT\APT.jar"
+
+#Location of the exported APT preferences file (you must set up the photometry settings in APT first)
 preferences = "APT.pref"
 
+
+#---------------------------------------------------------------------------------------------------
+
+
+
+#Loop through files in directory
 for filename in os.listdir(directory):
+
+	#Check it's a FITS file
 	if filename.endswith(".fit") or filename.endswith(".fits") or filename.endswith(".fts"): 
 		
+		#Full path to the file
 		file = os.path.join(directory, filename)
-		ofile = os.path.join(directory, os.path.splitext(filename)[0]+".tbl")
 		
 		print(file)
 		
+		#name of the output table file (source list)
+		ofile = os.path.join(directory, os.path.splitext(filename)[0]+".tbl")
+	
+		#parameters to feed APT
 		parameters = "-i \"" + file + "\" -s sourceListByAPT -o \"" + ofile + "\" "
 		
+		#Call APT
 		subprocess.call(["java", "-Duser.language=en", "-Duser.region=US", "-mx1024M", "-jar", aptJar, "-i", file, "-p", preferences, "-s", "sourceListByAPT", "-o", ofile])
 		
 		continue
@@ -22,3 +49,4 @@ for filename in os.listdir(directory):
 		continue
 
 
+print("Done!")
--- SourceLists.py
+++ SourceLists.py
@@ -1,109 +1,136 @@
+##### ----SourceLists.py---##########################################################################
+#																									#
+# Extracts specific sources from APT source lists - run after first running APTbatch.py 			#
+#																									#
+#####################################################################################################
+
 import os
 import math
 from astropy.io import fits
 
-##-------------------------------------------------------
-##--- Variables to be customised
+#-----------Variables to be customised before use-----------------------------------------------------
 
-directory = r"C:\Users\David\Documents\apt tests\fortuna"
+#Directory containing the FITs files and .tbl source lists from APT
+directory = r"C:\Users\David\Documents\apt tests\Transit 1"
 
-#The right ascension and declination of your target
+#The right ascension and declination of your target (decimal degrees)
 RA_target_1 = 129.328754
 DEC_target_1 = 16.082509
+
+
+
+#The right ascension and declination of the reference star (decimal degrees)
+RA_reference = 129.439444
+DEC_reference = 16.027707
+
+#The right ascension and declination of the check star/s (decimal degrees)
+RA_check_1 = 349.100001
+DEC_check_1 = 31.437105
+
+#Optional coordinates for a second check star (or 'None' if not used) (decimal degrees)
+RA_check_2 = None
+DEC_check_2 = None
+
+#Maximum distance from expected coordinates for a source to be considered (arcseconds)
+maxDist = 2
+
+#Name of the output file
+listFile = "Transit 1.csv"
 
 #Is the target moving? (True or False)
 movingTarget = True
 
-#If your target is moving:
+#If so, also edit the following:
 
-#The right ascension and declination of your target at a second point
+#The right ascension and declination of your moving target at a second point (decimal degrees)
 RA_target_2 = 129.360391
 DEC_target_2 = 16.072311
 
-#Julian date at the first and last point
+#Julian date at the first and last point (decimal degrees)
 JD_1 = 2458174.5500694443
 JD_2 = 2458174.3645023149
 
-#The right ascension and declination of the reference star
-RA_reference = 129.439444
-DEC_reference = 16.027707
 
-#The right ascension and declination of the check star/s
-RA_check_1 = 349.100001
-DEC_check_1 = 31.437105
+#Start of code ---------------------------------------------------------------------------------------------------
 
-#Coordinates for a second check star (or 'None' if not used)
-RA_check_2 = 348.874724
-DEC_check_2 = 31.415189
-
-#Maximum distance from expected coordinates for a source to be considered
-maxDist = 2
-
-#Name of the output file
-listFile = "output_asteroid.csv"
-
-
-##-------------------------------------------------------
-
+#Function to sort by the first element
 def sortFirst(val): 
     return val[0]  
 
+#Open the output file for writing
 f= open(listFile,"w")
+
+#Write the header information
 f.write("Julian Date,Target Intensity,Target Intensity Uncertainty,Target magnitude,target magnitude uncertainty,Reference Intensity,Reference Intensity Uncertainty,Reference magnitude,Reference magnitude uncertainty,Check1 Intensity,Check1 Intensity Uncertainty,Check1 magnitude,Check1 magnitude uncertainty")
+
+#If it has two check stars, add extra headers
 if RA_check_2 is None:
 	f.write("\n")
 else:
 	f.write(",Check2 Intensity,Check2 Intensity Uncertainty,Check2 magnitude,Check2 magnitude uncertainty\n")
+	
+#Close the output file
 f.close()
+
+#Initialise the list
+list = []
 
 i = 0
 
-list = []
-
+#If the target if moving, calculate the rate of movement from the start and end points
 if movingTarget == True:
-
+	
+	#Movement between the start and end points
 	raMovement = RA_target_2-RA_target_1
 	decMovement = DEC_target_2-DEC_target_1
+	
+	#Difference in days between the start and end points
 	timeScale = JD_2-JD_1
+	
+	#Rate of movement in degrees / days
 	raRate = raMovement/timeScale
 	decRate = decMovement/timeScale
 	
-	print "timeScale "+str(timeScale)+" raRate "+str(raRate)+" raRate "+str(decRate)
+	print "TimeScale "+str(timeScale)+" raRate "+str(raRate)+" raRate "+str(decRate)
 
 
-#loop through all files in directory
+#Loop through files in directory
 for filename in os.listdir(directory):
-	##check it's a fits file
+
+	#Check it's a FITS file
 	if filename.endswith(".fit") or filename.endswith(".fits") or filename.endswith(".fts"): 
 		
-		#full path to file
+		#Full path to the file
 		file = os.path.join(directory, filename)
 		
 		print file
 		
-		#path to source list
+		#Name of the table file (source list)
 		sourceList = os.path.join(directory, os.path.splitext(filename)[0]+".tbl")
 
-		#fits header data unit
+		#FITS header data unit
 		hdul = fits.open(file)
 
-		#get the Julian date
-		
+		#Get the Julian date from the header		
 		JD = float(hdul[0].header['JD'])	
 		
+		#Add the Julian date to the list of images
 		list.append([JD, 99,"0,0,0,0","0,0,0,0","0,0,0,0","0,0,0,0"])
 			
-		
+		#If it's a moving target, calculate the position
 		if movingTarget == True:
-			#julian date interpolation
 			
+			#Time offset from the first reference date
 			timeScale = JD-JD_1
 			
+			#Position of the target in this image
 			RA_target = RA_target_1 + (raRate*timeScale)
 			DEC_target = DEC_target_1 + (decRate*timeScale)
 			
 			print(str(timeScale)+" "+str(RA_target)+" "+str(DEC_target))
+			
 		else:
+			
 			RA_target = RA_target_1
 			DEC_target = DEC_target_1
 			
@@ -111,80 +138,86 @@ for filename in os.listdir(directory):
 		#initialise some variables	
 		lineNum=0
 
-		closestTarget = 99
-		closestRef = 99
-		closestCheck1 = 99
-		closestCheck2 = 99
+		closestTarget = 360
+		closestRef = 360
+		closestCheck1 = 360
+		closestCheck2 = 360
 
-		#open the source list for reading
+		#Open the corresponding source list for reading
 		sourceList_in = open(sourceList, "r")
 		
-		#loop through all lines in the file
+		#Loop through all lines in the file
 		for line in sourceList_in:
 		
-			#ignore the header lines
+			#Ignore the header lines
 			if lineNum >2:
 				
-				#split the line by white spaces
+				#Split the line by white spaces
 				parts = line.split()
 				
-				#ignore the last line
+				#Ignore the last line
 				if parts[0]!="End":
 				
-					ra = float(parts[3])
-					dec = float(parts[4])
-					Intensity = parts[10]
-					dIntensity = parts[12]
-					mag = parts[14]
-					dmag = parts[15]
-					fwhm = parts[35]
+					#Extract useful values from the source list
+					ra = float(parts[3]) 	#Right ascensions (degrees)
+					dec = float(parts[4]) 	#Declination (degrees)
+					Intensity = parts[10]	#Source intensity
+					dIntensity = parts[12]	#Intensity uncertainty
+					mag = parts[14]			#Source instrument magnitude
+					dmag = parts[15]		#magnitude uncertainty
+					fwhm = parts[35]		#Full width half maximum
 					
+					#Values to output
 					output_string =Intensity+","+dIntensity+","+mag+","+dmag
 							
+					#Convert the coordinates to radians		
 					ra1 = math.radians(RA_target)
 					ra2 = math.radians(ra)
 					d1 = math.radians(DEC_target)
 					d2 = math.radians(dec)			
 					
-					#calculate the distance from this source to the target
+					#Calculate the distance from this source to the target
 					angSep = 3600*math.degrees(math.acos(math.sin(d1)*math.sin(d2)+math.cos(d1)*math.cos(d2)*math.cos(ra1-ra2)))
-										
+						
+					#If it's close enough, and closer than the closest source so far, make it the new best source
 					if angSep < maxDist and angSep<closestTarget:
+					
 						closestTarget = angSep
 						list[i][1]=closestTarget
 						list[i][2]=output_string
-						
+					
+					#Convert the coordinates to radians	
 					ra1 = math.radians(RA_reference)
-					ra2 = math.radians(ra)
 					d1 = math.radians(DEC_reference)
-					d2 = math.radians(dec)			
 					
 					#calculate the distance from this source to the reference star
 					angSep = 3600*math.degrees(math.acos(math.sin(d1)*math.sin(d2)+math.cos(d1)*math.cos(d2)*math.cos(ra1-ra2)))
 										
+					#If it's close enough, and closer than the closest source so far, make it the new best source
 					if angSep < maxDist and angSep<closestRef:
 						closestRef = angSep
 						list[i][1]=closestRef
 						list[i][3]=output_string	
 						
+					#Convert the coordinates to radians	
 					ra1 = math.radians(RA_check_1)
-					ra2 = math.radians(ra)
 					d1 = math.radians(DEC_check_1)
-					d2 = math.radians(dec)			
 					
 					#calculate the distance from this source to the check star
 					angSep = 3600*math.degrees(math.acos(math.sin(d1)*math.sin(d2)+math.cos(d1)*math.cos(d2)*math.cos(ra1-ra2)))
 										
+					#If it's close enough, and closer than the closest source so far, make it the new best source
 					if angSep < maxDist and angSep<closestCheck1:
 						closestCheck1 = angSep
 						list[i][1]=closestCheck1
 						list[i][4]=output_string	
 					
+					#If there's a second check star
 					if RA_check_2 is not None:
+					
+						#Convert the coordinates to radians	
 						ra1 = math.radians(RA_check_2)
-						ra2 = math.radians(ra)
 						d1 = math.radians(DEC_check_2)
-						d2 = math.radians(dec)			
 						
 						#calculate the distance from this source to the second check star
 						angSep = 3600*math.degrees(math.acos(math.sin(d1)*math.sin(d2)+math.cos(d1)*math.cos(d2)*math.cos(ra1-ra2)))
@@ -196,28 +229,31 @@ for filename in os.listdir(directory):
 						
 					
 						
-					
+			#Next line		
 			lineNum=lineNum+1
 
-
+		#Close the current source list
 		sourceList_in.close()
 		
 		i=i+1
 		
-#sort by julian date
+#Sort by Julian Date
 list.sort(key = sortFirst)  
 		
+#Open the output file (for appending)
 f= open(listFile,"a")			
 		
+#Loop through our list of images
 for n in range(i):
 
+	#Output the data
 	f.write(str(list[n][0])+","+list[n][2]+","+list[n][3]+","+list[n][4])
+	
+	#Add check star 2 if we have one
 	if RA_check_2 is None:
 		f.write("\n")
 	else:
 		f.write(","+list[n][5]+"\n")
 
-
+#Close the output
 f.close() 
-		
-#print list
