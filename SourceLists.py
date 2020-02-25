b34cdae4b3d9917aef24e4031bd98729b7233665
David Campbell <d.a.campbell2@herts.ac.uk>
2020-02-25 20:50:18 +0000

    Update SourceLists.py

--- SourceLists.py
+++ SourceLists.py
@@ -55,6 +55,10 @@ JD_2 = 2458174.3645023149
 def sortFirst(val): 
     return val[0]  
 
+#remove any previous output files to write new data
+if os.path.exists(listFile):
+	os.remove(listFile)	
+	
 #Open the output file for writing
 f= open(listFile,"w")
 
@@ -89,7 +93,7 @@ if movingTarget == True:
 	raRate = raMovement/timeScale
 	decRate = decMovement/timeScale
 	
-	print "TimeScale "+str(timeScale)+" raRate "+str(raRate)+" raRate "+str(decRate)
+	print ("TimeScale "+str(timeScale)+" raRate "+str(raRate)+" raRate "+str(decRate))
 
 
 #Loop through files in directory
@@ -101,139 +105,145 @@ for filename in os.listdir(directory):
 		#Full path to the file
 		file = os.path.join(directory, filename)
 		
-		print file
+		print (file)
 		
 		#Name of the table file (source list)
 		sourceList = os.path.join(directory, os.path.splitext(filename)[0]+".tbl")
-
-		#FITS header data unit
-		hdul = fits.open(file)
-
-		#Get the Julian date from the header		
-		JD = float(hdul[0].header['JD'])	
 		
-		#Add the Julian date to the list of images
-		list.append([JD, 99,"0,0,0,0","0,0,0,0","0,0,0,0","0,0,0,0"])
-			
-		#If it's a moving target, calculate the position
-		if movingTarget == True:
-			
-			#Time offset from the first reference date
-			timeScale = JD-JD_1
-			
-			#Position of the target in this image
-			RA_target = RA_target_1 + (raRate*timeScale)
-			DEC_target = DEC_target_1 + (decRate*timeScale)
-			
-			print(str(timeScale)+" "+str(RA_target)+" "+str(DEC_target))
-			
-		else:
-			
-			RA_target = RA_target_1
-			DEC_target = DEC_target_1
-			
-			
-		#initialise some variables	
-		lineNum=0
+		#check the file exists
+		if os.path.exists(sourceList):			
 
-		closestTarget = 360
-		closestRef = 360
-		closestCheck1 = 360
-		closestCheck2 = 360
+			#FITS header data unit
+			hdul = fits.open(file)
 
-		#Open the corresponding source list for reading
-		sourceList_in = open(sourceList, "r")
-		
-		#Loop through all lines in the file
-		for line in sourceList_in:
-		
-			#Ignore the header lines
-			if lineNum >2:
+			#Get the Julian date from the header		
+			JD = float(hdul[0].header['JD'])	
+			
+			#Add the Julian date to the list of images
+			list.append([JD, 99,"0,0,0,0","0,0,0,0","0,0,0,0","0,0,0,0"])
 				
-				#Split the line by white spaces
-				parts = line.split()
+			#If it's a moving target, calculate the position
+			if movingTarget == True:
 				
-				#Ignore the last line
-				if parts[0]!="End":
+				#Time offset from the first reference date
+				timeScale = JD-JD_1
 				
-					#Extract useful values from the source list
-					ra = float(parts[3]) 	#Right ascensions (degrees)
-					dec = float(parts[4]) 	#Declination (degrees)
-					Intensity = parts[10]	#Source intensity
-					dIntensity = parts[12]	#Intensity uncertainty
-					mag = parts[14]			#Source instrument magnitude
-					dmag = parts[15]		#magnitude uncertainty
-					fwhm = parts[35]		#Full width half maximum
+				#Position of the target in this image
+				RA_target = RA_target_1 + (raRate*timeScale)
+				DEC_target = DEC_target_1 + (decRate*timeScale)
+				
+				print(str(timeScale)+" "+str(RA_target)+" "+str(DEC_target))
+				
+			else:
+				
+				RA_target = RA_target_1
+				DEC_target = DEC_target_1
+				
+				
+			#initialise some variables	
+			lineNum=0
+
+			closestTarget = 360
+			closestRef = 360
+			closestCheck1 = 360
+			closestCheck2 = 360
+
+			#Open the corresponding source list for reading
+			sourceList_in = open(sourceList, "r")
+			
+			#Loop through all lines in the file
+			for line in sourceList_in:
+			
+				#Ignore the header lines
+				if lineNum >2:
 					
-					#Values to output
-					output_string =Intensity+","+dIntensity+","+mag+","+dmag
+					#Split the line by white spaces
+					parts = line.split()
+					
+					#Ignore the last line
+					if parts[0]!="End":
+					
+						#Extract useful values from the source list
+						ra = float(parts[3]) 	#Right ascensions (degrees)
+						dec = float(parts[4]) 	#Declination (degrees)
+						Intensity = parts[10]	#Source intensity
+						dIntensity = parts[12]	#Intensity uncertainty
+						mag = parts[14]			#Source instrument magnitude
+						dmag = parts[15]		#magnitude uncertainty
+						fwhm = parts[35]		#Full width half maximum
+						
+						#Values to output
+						output_string =Intensity+","+dIntensity+","+mag+","+dmag
+								
+						#Convert the coordinates to radians		
+						ra1 = math.radians(RA_target)
+						ra2 = math.radians(ra)
+						d1 = math.radians(DEC_target)
+						d2 = math.radians(dec)			
+						
+						#Calculate the distance from this source to the target
+						angSep = 3600*math.degrees(math.acos(math.sin(d1)*math.sin(d2)+math.cos(d1)*math.cos(d2)*math.cos(ra1-ra2)))
 							
-					#Convert the coordinates to radians		
-					ra1 = math.radians(RA_target)
-					ra2 = math.radians(ra)
-					d1 = math.radians(DEC_target)
-					d2 = math.radians(dec)			
-					
-					#Calculate the distance from this source to the target
-					angSep = 3600*math.degrees(math.acos(math.sin(d1)*math.sin(d2)+math.cos(d1)*math.cos(d2)*math.cos(ra1-ra2)))
+						#If it's close enough, and closer than the closest source so far, make it the new best source
+						if angSep < maxDist and angSep<closestTarget:
 						
-					#If it's close enough, and closer than the closest source so far, make it the new best source
-					if angSep < maxDist and angSep<closestTarget:
-					
-						closestTarget = angSep
-						list[i][1]=closestTarget
-						list[i][2]=output_string
-					
-					#Convert the coordinates to radians	
-					ra1 = math.radians(RA_reference)
-					d1 = math.radians(DEC_reference)
-					
-					#calculate the distance from this source to the reference star
-					angSep = 3600*math.degrees(math.acos(math.sin(d1)*math.sin(d2)+math.cos(d1)*math.cos(d2)*math.cos(ra1-ra2)))
-										
-					#If it's close enough, and closer than the closest source so far, make it the new best source
-					if angSep < maxDist and angSep<closestRef:
-						closestRef = angSep
-						list[i][1]=closestRef
-						list[i][3]=output_string	
+							closestTarget = angSep
+							list[i][1]=closestTarget
+							list[i][2]=output_string
 						
-					#Convert the coordinates to radians	
-					ra1 = math.radians(RA_check_1)
-					d1 = math.radians(DEC_check_1)
-					
-					#calculate the distance from this source to the check star
-					angSep = 3600*math.degrees(math.acos(math.sin(d1)*math.sin(d2)+math.cos(d1)*math.cos(d2)*math.cos(ra1-ra2)))
-										
-					#If it's close enough, and closer than the closest source so far, make it the new best source
-					if angSep < maxDist and angSep<closestCheck1:
-						closestCheck1 = angSep
-						list[i][1]=closestCheck1
-						list[i][4]=output_string	
-					
-					#If there's a second check star
-					if RA_check_2 is not None:
-					
 						#Convert the coordinates to radians	
-						ra1 = math.radians(RA_check_2)
-						d1 = math.radians(DEC_check_2)
+						ra1 = math.radians(RA_reference)
+						d1 = math.radians(DEC_reference)
 						
-						#calculate the distance from this source to the second check star
+						#calculate the distance from this source to the reference star
 						angSep = 3600*math.degrees(math.acos(math.sin(d1)*math.sin(d2)+math.cos(d1)*math.cos(d2)*math.cos(ra1-ra2)))
 											
-						if angSep < maxDist and angSep<closestCheck2:
-							closestCheck2 = angSep
-							list[i][1]=closestCheck2
-							list[i][5]=output_string	
+						#If it's close enough, and closer than the closest source so far, make it the new best source
+						if angSep < maxDist and angSep<closestRef:
+							closestRef = angSep
+							list[i][1]=closestRef
+							list[i][3]=output_string	
+							
+						#Convert the coordinates to radians	
+						ra1 = math.radians(RA_check_1)
+						d1 = math.radians(DEC_check_1)
 						
-					
+						#calculate the distance from this source to the check star
+						angSep = 3600*math.degrees(math.acos(math.sin(d1)*math.sin(d2)+math.cos(d1)*math.cos(d2)*math.cos(ra1-ra2)))
+											
+						#If it's close enough, and closer than the closest source so far, make it the new best source
+						if angSep < maxDist and angSep<closestCheck1:
+							closestCheck1 = angSep
+							list[i][1]=closestCheck1
+							list[i][4]=output_string	
 						
-			#Next line		
-			lineNum=lineNum+1
+						#If there's a second check star
+						if RA_check_2 is not None:
+						
+							#Convert the coordinates to radians	
+							ra1 = math.radians(RA_check_2)
+							d1 = math.radians(DEC_check_2)
+							
+							#calculate the distance from this source to the second check star
+							angSep = 3600*math.degrees(math.acos(math.sin(d1)*math.sin(d2)+math.cos(d1)*math.cos(d2)*math.cos(ra1-ra2)))
+												
+							if angSep < maxDist and angSep<closestCheck2:
+								closestCheck2 = angSep
+								list[i][1]=closestCheck2
+								list[i][5]=output_string	
+							
+						
+							
+				#Next line		
+				lineNum=lineNum+1
 
-		#Close the current source list
-		sourceList_in.close()
-		
-		i=i+1
+			#Close the current source list
+			sourceList_in.close()
+			
+			i=i+1
+			
+		else:
+			print("Could not find" + sourceList)
 		
 #Sort by Julian Date
 list.sort(key = sortFirst)  
